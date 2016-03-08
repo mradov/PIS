4d40c6acfa0f0e3837442e3d9fc9a1bb729c7808
avidjak <tonividak@hotmail.com>
2016-03-08 09:04:50 +0100

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Eeeeeeeeeeeeeeeeeeeeeej!"); //kakve su ovo poruke
+            Console.WriteLine("sdfghjk");
         }
     }
 }
